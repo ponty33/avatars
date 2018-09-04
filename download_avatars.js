8a018e2bc25e73e79c1522e49c26c0f225a7cc18
Frank Chung <ponty33@gmail.com>
2018-09-04 01:37:55 +0000

    finalize

--- download_avatars.js
+++ download_avatars.js
@@ -22,22 +22,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors(input[2], input[3], function(err, result) {
   for (var i = 0; i < result.length; i++) {
-    
     downloadImageByURL(result[i].avatar_url, 'avatars/' + result[i].login + '.jpg')
-
   }
-  
   console.log("Errors:", err);
 });
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-       .on('error', function (err) {                                  
-         throw err; 
-       })
-       .on('response', function (response) {
-         console.log('Response Status Code: ', response.statusCode);
-       })
-       .pipe(fs.createWriteStream('./' + filePath));    
+    .on('error', function (err) {                                  
+      throw err; 
+    })
+    .on('response', function (response) {
+      console.log('Response Status Code: ', response.statusCode);
+    })
+    .pipe(fs.createWriteStream('./' + filePath));    
 }
 
