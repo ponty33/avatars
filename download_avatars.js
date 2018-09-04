882826f710a4cc4b9508a06e187b58f79638b1fa
Frank Chung <ponty33@gmail.com>
2018-09-04 01:11:20 +0000

    step7

--- download_avatars.js
+++ download_avatars.js
@@ -32,7 +32,14 @@ getRepoContributors(input[2], input[3], function(err, result) {
 });
 
 function downloadImageByURL(url, filePath) {
-  
+  request.get(url)
+       .on('error', function (err) {                                  
+         throw err; 
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream('./' + filePath));    
 }
 
 downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
